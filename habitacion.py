19904e57dbbc45a2d6981b280512a279434e973a
Begonia Vela <167794679+BegoniaVela@users.noreply.github.com>
2025-10-13 02:07:36 -0500

    Update habitacion.py

--- habitacion.py
+++ habitacion.py
@@ -1,12 +1,11 @@
-class Habitacion():
-    def __init__(self,numero, precio):
-        self.__numero = numero
-        self.__precio = precio
+class Habitacion:
+    def __init__(self, numero_habitacion, precio_noche):
+        self.__numero_habitacion = numero_habitacion
+        self.__precio_noche = precio_noche
 
     def __str__(self):
-        return f"Habitacion Numero: {self.__numero} , Precio: {self.__precio}"
-    
-    def GetPrecio(self):
-        return self.__precio
-        
+        return f"Habitación N°: {self.__numero_habitacion}, Precio por noche: S/ {self.__precio_noche}"
+
+    def get_precio(self):
+        return self.__precio_noche
     
